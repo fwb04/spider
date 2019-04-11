db73ec69c0e06612ad4be12f34a0735885060801
Fang Wenbing <xhdzz0420@mail.ustc.edu.cn>
2019-04-11 21:57:10 +0800

    write sqlite

--- spydersql/spysql.py
+++ spydersql/spysql.py
@@ -3,19 +3,19 @@
 import requests
 import json
 import time
+import sqlite3
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 
 def gettime():
     return int(round(time.time() * 1000))
 
-if __name__ == '__main__':
-
+def getpopulation():
     # 用来自定义头部的
     headers = {}
     # 用来传递参数的
     keyvalue = {}
-    # 目标网址(问号前面的东西)
+    # 目标网址
     url = 'http://data.stats.gov.cn/easyquery.htm'
 
     # 头部的填充
@@ -23,7 +23,7 @@ if __name__ == '__main__':
                             'AppleWebKit/605.1.15 (KHTML, like Gecko) ' \
                             'Version/12.0 Safari/605.1.15'
 
-    # 下面是参数的填充
+    # 参数的填充
     keyvalue['m'] = 'QueryData'
     keyvalue['dbcode'] = 'hgnd'
     keyvalue['rowcode'] = 'zb'
@@ -36,19 +36,19 @@ if __name__ == '__main__':
     s = requests.session()
     # 在Session基础上进行一次请求
     r = s.get(url, params=keyvalue, headers=headers)
-    # 打印返回过来的状态码
-    print(r.status_code)
+
     # 修改dfwds字段内容
     keyvalue['dfwds'] = '[{"wdcode":"sj","valuecode":"LAST20"}]'
     # 再次进行请求
     r = s.get(url, params=keyvalue, headers=headers)
-    # 此时我们就能获取到我们搜索到的数据了
-    # print(r.text)
 
+    # 定义人口数据存储数组
     year = []
     population = []
     male = []
     female = []
+
+    # 从json文件提取想要的数据
     data = json.loads(r.text)
     data_one = data['returndata']['datanodes']
     for value in data_one:
@@ -65,23 +65,61 @@ if __name__ == '__main__':
     print(male)
     print(female)
 
+    # 数组逆序存放
     year.reverse()
     population.reverse()
     male.reverse()
     female.reverse()
 
-    # fig = plt.figure()  # 创建画布
-    # ax = plt.subplot()  # 创建图片区域
-    # plt.grid(color='lightgrey', ls='--')
-    # plt.ylim(125000, 140000)
-    # plt.rcParams['font.sans-serif'] = ['SimHei']
-    # plt.rcParams['axes.unicode_minus'] = False
-    # ax.bar(year, population, align='edge', color='sandybrown', edgecolor='white')
-    # plt.xlabel(u'年份')
-    # plt.ylabel(u'万人')
-    # plt.title(u'1999-2018年末总人口')
-    # plt.show()
-    #
-    # plt.plot(year, male)
-    # plt.plot(year, female)
-    # plt.show()
+    # 连接数据库，不存在时自动创建
+    conn = sqlite3.connect("population.db")
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS population
+              (year text, popu int, male int, female int)''')
+    for i in range(len(year)):
+        cur.execute("INSERT INTO population VALUES ('%s','%d','%d','%d')" % (year[i], population[i], male[i], female[i]))
+    # cur.execute("INSERT INTO population VALUES ('2009',1,2,3)")
+    # y = ('2009',)
+    # cur.execute('SELECT * FROM population WHERE year=?', y)
+    # print(cur.fetchone())
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def plotdata():
+    conn = sqlite3.connect("population.db")
+    cur = conn.cursor()
+    cur.execute('select * from population ')
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    year = [i[0] for i in data]
+    population = [i[1] for i in data]
+    male = [i[2] for i in data]
+    female = [i[3] for i in data]
+
+    fig = plt.figure()  # 创建画布
+    ax = plt.subplot()  # 创建图片区域
+    plt.grid(color='lightgrey', ls='--')
+    plt.ylim(125000, 140000)
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    ax.bar(year, population, align='edge', color='sandybrown', edgecolor='white')
+    plt.xlabel(u'年份')
+    plt.ylabel(u'万人')
+    plt.title(u'1999-2018年末总人口')
+    plt.show()
+
+    plt.plot(year, male)
+    plt.plot(year, female)
+    plt.show()
+
+
+if __name__ == '__main__':
+
+    # 从国家统计局获取人口数据并存入本地数据库population.db
+    getpopulation()
+
+    # 作图
+    plotdata()
